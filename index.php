37175991c48a48b753d67fcbf4cfdb8dc8ba6e00
Alexandra Onegård <onegard@kth.se>
2018-05-04 10:28:31 +0200

    Start på hemsidan

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Sternax</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="JS/navbar.js" type="text/javascript"></script>
+  <!-- CSS styles -->
+  <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="CSS/styleDiv.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top role="navigation">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">STERNAX DATA AB</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">HEM</a></li>
+      <li><a href="#services">TJÄNSTER</a></li>
+      <li><a href="#about">OM OSS</a></li>
+      <li><a href="#team">ANSTÄLLDA</a></li>
+      <li><a href="#clients">KUNDER</a></li>
+      <li><a href="#contact">KONTAKTA OSS</a></li>
+    </ul>
+  </div>
+</nav>
+
+<!-- Header -->
+<div class="container-fluid special">
+  <div class="jumbotron">
+  <header class="text-center" name="home">
+    <div class="intro-text">
+      <h1>Välkommen till <strong>Sternax Data AB</strong></h1>
+      <p>För kunden rätt konsult</p>
+      <a href="#services" class="btn btn-default btn-lg page-scroll">Läs mer</a> 
+    </div>
+  </header>
+  </div>
+
+  <div class="jumbotron">
+  	<div class="text-center">
+  	  <h2>TJÄNSTER</h2>
+  	<hr>
+  	<p>Här kan ni läsa mer om våra tjänster. </p>
+  </div>
+  <div class="space"></div>
+  <div class="row">
+      <div class="col-md-3 col-sm-4 col-lg-4 service"> <i class="fa fa-desktop"></i>
+        <h4>Web Design</h4>
+        <p>Vi hjälper till med design och utveckling av er webtjänst.</p>
+      </div>
+      <div class="col-md-3 col-sm-4 col-lg-4 service"> <i class="fa fa-gears"></i>
+        <h4>Android App Development</h4>
+        <p>Vi har utvecklat ett antal s.k. Appar för Android enheter.</p>
+      </div>
+      <div class="col-md-3 col-sm-4 col-lg-4 service"> <i class="fa fa-line-chart"></i>
+        <h4>Software development</h4>
+        <p>Vi stödjer er i ert systemutvecklingsprojekt oavsett verktyg. Vi har mångårig erfarenhet av Java, .Net, Salesforce m.fl. plattformar.</p>
+      </div>
+    </div>
+  </div>
+<div class="jumbotron" id="about">OM OSS</div>
+<div class="jumbotron" id="team">ANSTÄLLDA</div>
+<div class="jumbotron" id="clients">KUNDER</div>
+<div class="jumbotron" id="contact">KONTAKTA OSS</div>
+</div>
+
+<footer class="navbar navbar-inverse navbar-fixed-bottom">
+    <!--Social buttons-->
+    <div class="text-center">
+        <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+                <a class="btn btn-social-icon btn-facebook" href="https://www.facebook.com/Sternax-Data-224338531640694/"><i class="fa fa-facebook-square" style="font-size:30px; color: orange"> </i></a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn btn-social-icon btn-facebook"><i class="fa fa-google-plus" style="font-size:30px; color: orange"> </i></a>
+            </li>
+        </ul>
+    </div>
+    <!--/.Social buttons-->
+
+<!-- Footer copyright -->
+<div class="footer-copyright py-3 text-center">
+  © 2018 Copyright: 
+  <a href="sternax.se">STERNAX DATA AB </a>, Org.nr. 556920-0339
+</div>
+<!-- /.Copyright -->
+</footer>
+<!-- /.Footer -->
+
+</body>
+</html>
